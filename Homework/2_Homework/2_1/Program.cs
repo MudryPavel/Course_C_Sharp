888e15444ee54fcb339392e713c693065a2d6cd9
Pavel <nemudrov98@mail.ru>
2022-07-15 01:07:47 +0300

    Homework 2_1

---
+++ Homework/2_Homework/2_1/Program.cs
@@ -0,0 +1,8 @@
+﻿// Напишите программу, которая принимает на вход трёхзначное число и на выходе показывает вторую цифру этого числа.
+int SecondDigit(int num)
+{
+    return(((num%100)-(num%10))/10);
+}
+Console.WriteLine("Write a 3-digit number a: ");
+int a = int.Parse(Console.ReadLine());
+Console.WriteLine(SecondDigit(a));
