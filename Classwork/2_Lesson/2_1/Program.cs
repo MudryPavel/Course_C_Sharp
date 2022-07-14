cc6af139e815bba110152e2ec070a687e08ace97
Pavel <nemudrov98@mail.ru>
2022-07-15 00:40:34 +0300

    ClassWork 2_1

--- Classwork/2_Lesson/2_1/Program.cs
+++ Classwork/2_Lesson/2_1/Program.cs
@@ -1,2 +1,6 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿//Напишите программу, которая принимает на вход трёхзначное число и на выходе показывает последнюю цифру этого числа.
+int LastDigit(int num)
+{
+    return(num%10);
+}
+Console.WriteLine(LastDigit(678));
