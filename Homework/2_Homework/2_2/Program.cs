806889805b1e2cfdf928375cf76d855bc0e70df3
Pavel <nemudrov98@mail.ru>
2022-07-15 01:13:42 +0300

    Homework 2_2

---
+++ Homework/2_Homework/2_2/Program.cs
@@ -0,0 +1,8 @@
+﻿// Напишите программу, которая выводит случайное трёхзначное число и удаляет вторую цифру этого числа.
+int SecondDigitDeletion(int num)
+{
+    return((num/100)*10+(num%10));
+}
+Console.WriteLine("Write a 3-digit number a: ");
+int a = int.Parse(Console.ReadLine());
+Console.WriteLine(SecondDigitDeletion(a));
