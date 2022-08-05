5b7b9243d622e374342bc2b10bc5d964fd3ad485
Pavel <nemudrov98@mail.ru>
2022-08-05 11:13:29 +0300

    Add 5_1_1, 5_2

---
+++ Classwork/5_Lesson/5_2/Program.cs
@@ -0,0 +1,33 @@
+﻿int[] CreateArray()
+{
+    Console.WriteLine("Input number of elements");
+    int num = int.Parse(Console.ReadLine());
+    Console.WriteLine("Input elemet's min");
+    int min = int.Parse(Console.ReadLine());
+    Console.WriteLine("Input elemet's max");
+    int max = int.Parse(Console.ReadLine());
+    int[] array = new int[num];
+    for(int i=0; i < num; i++)
+    {
+        array[i]=new Random().Next(min, max+1);
+        Console.Write($"{array[i]} ");
+    }
+    return(array);
+}
+void ArrayElementCounter(int[] array)
+{
+    Console.WriteLine("Input counting value");
+    int s = int.Parse(Console.ReadLine());
+    int a=0;
+    for(int i=0; i < array.Length; i++)
+    {
+        
+        if(array[i]==s)
+            a+=1;     
+    }
+    if (s==0)
+        Console.WriteLine("This value isn't found in array");
+    else
+        Console.WriteLine($"This value is found in array {a} times");
+}
+ArrayElementCounter(CreateArray());    
