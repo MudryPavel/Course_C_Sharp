51d5c10e51903bd8b5c0ef524169b00f16398d91
Pavel <nemudrov98@mail.ru>
2022-08-12 10:45:20 +0300

    Add 7_1, 7_1_1, 7_2

---
+++ Classwork/7_Lesson/7_1_1/Program.cs
@@ -0,0 +1,34 @@
+﻿void Print2xArray(int[,] arr)
+{
+    int row_size = arr.GetLength(0);
+    int column_size = arr.GetLength(1);
+
+    for(int i=0; i<row_size; i++)
+    {
+        for(int j=0; j<column_size; j++)
+        {
+            Console.Write($"{arr[i, j]} ");
+        }
+        Console.WriteLine();
+    }
+    Console.WriteLine();
+}
+
+int[,] Fill2xArray(int row, int column, int min, int max)
+{
+    int[,] arr = new int[row,column];
+    
+    for(int i=0; i<row; i++)
+    {
+        for(int j=0; j<column; j++)
+        {
+            arr[i,j]=i+j;
+        }
+        Console.WriteLine();
+    }
+    Console.WriteLine();
+    return(arr);
+}
+
+int[,] arr1 = Fill2xArray(4,4,-10,10);
+Print2xArray(arr1);
