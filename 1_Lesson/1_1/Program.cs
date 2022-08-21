c05f932ac76ed896db75d352208f8cf0c5dd96c7
MudryPavel <108515067+MudryPavel@users.noreply.github.com>
2022-08-21 14:45:33 +0300

    Delete 1_Lesson directory

--- 1_Lesson/1_1/Program.cs
+++
@@ -1,6 +0,0 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Write a number: ");
-string s_a = Console.ReadLine();
-
-int a = int.Parse(s_a);
-Console.WriteLine(a * a);
