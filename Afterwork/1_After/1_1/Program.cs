151d5970fea1bf173f4776469dbb37023528ad30
Pavel <nemudrov98@mail.ru>
2022-08-26 21:56:10 +0300

    Change2 1_1

--- Afterwork/1_After/1_1/Program.cs
+++ Afterwork/1_After/1_1/Program.cs
@@ -69,38 +69,44 @@ int []DirectionChange(int[]direction)
     }
     return direction;
 }
-// int [,] SpiralFill2xArray(int[,] array)
-// {
+
+int [,] Spiral2xArray(int row, int column)
+{ 
+    int[,] array = new int[row,column];
     
-//     int row_size = array.GetLength(0); 
-//     int column_size = array.GetLength(1);
-//     int i=0;
-//     int j=0;
-//     int[]direction ={0,1};
+    for(int k=0; k<row; k++)
+    {
+        for(int l=0; l<column; l++)
+        {
+            array[k,l]=0;
+        }
+    }
+        
+    int i=0;
+    int j=0;
+    int iNew=0;
+    int jNew=0;
+    int[]direction ={0,1};
    
-//     for (int Index=1;Index<=row_size*column_size;Index++)
-//     {
-//         if(i>=0 && i<array.GetLength(0) && j>=0 && j<array.GetLength(1) && array[i,j]==0)
-//         {
-//             array[i,j]=Index;
-//             i+=direction[0];
-//             j+=direction[1];
-//         }
-//         else
-//         {
-//             i-=direction[0];
-//             j-=direction[1];
-//             DirectionChange(direction);
-//             i+=direction[0];
-//             j+=direction[1];
-//             array[i,j]=Index;
-//             i+=direction[0];
-//             j+=direction[1];
-//         }
-               
-//    }
-//    return array;
-// }
+    for (int Index=1;Index<=row*column;Index++)
+    {
+        array[i,j]=Index;
+        iNew=i+direction[0];
+        jNew=j+direction[1];
+        if(iNew>=0 && iNew<row && jNew>=0 && jNew<column && array[iNew,jNew]==0)
+        {
+            i+=direction[0];
+            j+=direction[1];
+        }
+        else
+        {
+            DirectionChange(direction);
+            i+=direction[0];
+            j+=direction[1];
+        }
+    }
+   return array;
+}
 
 int [,] SpiralFill2xArray(int[,] array)
 { 
@@ -132,9 +138,7 @@ int [,] SpiralFill2xArray(int[,] array)
    return array;
 }
 
-
-
 int[,] Array1= Fill2xArray(5,5,0,0);
 Print2xArray(Array1);
-SpiralFill2xArray(Array1);
-Print2xArray(Array1);
+int[,] Array2 = Spiral2xArray(5,5);
+Print2xArray(Array2);
